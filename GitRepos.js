20efddad9862a443a6ba5675086783c0121474d8
Sourajit Karada <sourajitkarada@gmail.com>
2019-02-14 01:15:48 +0530

    git repos project structure

---
+++ GitRepos.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class GitRepos extends Component {
+  render() {
+    return (
+      <View>
+        <Text>GitRepos</Text>
+      </View>
+    );
+  }
+}
+
+export default GitRepos;
